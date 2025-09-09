4bbd9f2934355402f8266a29993f8ab5f57187b5
ssp24 <62755563+ssp24@users.noreply.github.com>
2025-09-09 17:49:21 +0200

    Upload

---
+++ app/app_functions.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+# Loading Images:
+def resource_path(relative_path):
+    base_path = getattr(sys, '_MEIPASS', os.path.abspath("."))
+    return os.path.join(base_path, relative_path)
